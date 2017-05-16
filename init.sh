f49543870c665b12a9375e865f14766313f93d45
puppycodes <rye@jodie.io>
2017-05-15 21:52:23 -0700

    ok let it do its thing

--- init.sh
+++ init.sh
@@ -14,7 +14,7 @@ docker pull puppycodes/srs > /dev/null
 echo "done."
 
 echo "=> Starting Docker container..."
-CID=$(docker run --restart=always -p 80:8080 -v /Users/rye/repos/srs-theme:/usr/share/nginx/www/storage/themes/srs -d puppycodes/srs /sbin/my_init)
+CID=$(docker run --restart=always -p 80:8080 -v docker run --restart=always -p 80:8080 -v /data/koken/www:/usr/share/nginx/www -v /data/koken/mysql:/var/lib/mysql -d puppycodes/srs /sbin/my_init)
 
 echo -n "=> Waiting for Koken to become available.."
 
