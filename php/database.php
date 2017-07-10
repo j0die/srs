9cd0c10ad2163ca77a1ff2ef0e0f8b8f1d5a9fb8
puppycodes <rye@jodie.io>
2017-07-10 07:04:42 -0700

    getting ther

--- php/database.php
+++ php/database.php
@@ -2,7 +2,7 @@
 
 	$KOKEN_DATABASE = array(
 		'driver' => 'mysqli',
-		'hostname' => 'localhost',
+		'hostname' => '127.0.0.1',
 		'database' => 'koken',
 		'username' => 'koken',
 		'password' => 'prior-firewood-terrain-catcall-scrim-mote',
